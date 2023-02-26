ea107060f3f5019d3bf8062c0083cd41a91a2c17
abhishekinfrrd <112868050+abhishekinfrrd@users.noreply.github.com>
2023-02-26 19:02:54 +0530

    cart added

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,9 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input ,OnInit,Output,EventEmitter} from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-box',
   templateUrl:'./product-box.component.html'
 })
-export class ProductBoxComponent {
+export class ProductBoxComponent implements OnInit{
   @Input() fullWidthMode = false;
+
+  product: Product | undefined = {
+    id:1,
+    title:"Sneakers",
+    price:2000,
+    category : "shoes",
+    description : "sddddddddddddddddd",
+    image : "https://via.placeholder.com/150"
+  };
+
+  @Output() addToCart = new EventEmitter();
+
+  ngOnInit(): void {
+    
+  }
+  onAddToCart():void{
+    this.addToCart.emit(this.product);
+  }
 }
